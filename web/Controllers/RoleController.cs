19b91af9f4ab59760b27c1ce05a466133005a501
Belov Boris <BelovBoriss@yandex.ru>
2021-01-28 13:17:45 +0300

    Фикс описания сваггера

--- web/Controllers/RoleController.cs
+++ web/Controllers/RoleController.cs
@@ -75,7 +75,7 @@ namespace web.Controllers
         [HttpGet]
         [Authorize]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<SearchResponseDto<CompanyDto>>> Find([FromQuery] RoleSearchParameters searchParameters, CancellationToken cancellationToken)
+        public async Task<ActionResult<SearchResponseDto<RoleDto>>> Find([FromQuery] RoleSearchParameters searchParameters, CancellationToken cancellationToken)
         {
             var result = await this.roleService.FindAsync(searchParameters, cancellationToken);
 

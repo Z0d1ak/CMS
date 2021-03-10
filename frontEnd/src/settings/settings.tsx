5ab2fe3b394b648c34d687d1d2344d8a74ac1d3d
Dubinich <dodubina@gmail.com>
2021-03-10 04:38:36 +0300

    refactoring

--- frontEnd/src/settings/settings.tsx
+++ frontEnd/src/settings/settings.tsx
@@ -3,7 +3,9 @@ import './settings.css';
 import 'antd/dist/antd.css';
 import {Layout,BackTop,Row,Col,Button} from 'antd';
 import {
-    RollbackOutlined
+    RollbackOutlined,
+    CloseOutlined,
+    PlusOutlined
 } from '@ant-design/icons';
 import {
     Link,
@@ -12,6 +14,8 @@ import {
   } from "react-router-dom";
 
   import {useHistory} from "react-router-dom";
+  import {Dropdown, Menu, message, Typography,Space} from "antd";
+const { Paragraph } = Typography;
   
 /**
  * Класс компонента настроек пользователя
@@ -53,6 +57,10 @@ export class Settings extends React.Component<{},{}> {
                             <Col span={1}></Col>
                             Здесь будет настройки сервиса
                         </Row>
+                        <Row>
+                            <Col span={1}></Col>
+                            <RolesList data={["a","b","c","d"]}></RolesList>
+                        </Row>
                         <BackTop>
                             <div className="BackUp">Вверх</div>
                         </BackTop>
@@ -60,6 +68,75 @@ export class Settings extends React.Component<{},{}> {
             </Layout>
             );
 
+    }
+}
+
+export class RolesList extends React.Component<{data:string[]},{}> {
+
+    state=
+    {
+        data:this.props.data
+    }
+
+    deleteUl(id:number):string[] {
+        console.log("del"+id);
+        let updateArray = [...this.state.data];
+        updateArray.splice(id, 1);
+        console.log(updateArray);
+        this.setState({data:updateArray});
+        return updateArray;
+    }
+
+    addUl(el:string):string[] {
+        console.log("add"+el);
+        let updateArray = [...this.state.data];
+        updateArray.splice(this.state.data.length,0,el );
+        console.log(updateArray);
+        this.setState({data:updateArray});
+        return updateArray;
+    }
+
+    updateOptionsMenuCallBack():JSX.Element {
+        let optList:string[]=["SuperAdmin","CompanyAdmin","ChiefRedactor","Redactor","Author","Corrector"]
+        return <Menu>
+            {optList.map((r, i) => {
+                if(this.state.data.indexOf( r ) == -1 )
+                return (
+                    
+                    <Menu.Item onClick={()=>this.addUl(r)}>
+                        {r}
+                    </Menu.Item>
+                )
+            })}
+        </Menu>
+        
+    }
+
+    render() {
+     
+     return(
+        <div>
+            {this.state.data.map((d, i) => {
+                return (
+                <Button  key={i+"dl"} className="deleteButton" danger
+                                    type="dashed"
+                                    onClick={()=>this.deleteUl(i)}
+                  >
+                        {d} <CloseOutlined />
+                </Button>
+                )
+            })}
+            <Dropdown overlay={this.updateOptionsMenuCallBack()}>
+            <Button
+                type="dashed"
+                onClick={() => {}}
+                style={{ width: '100%' }}
+                >
+                Добавить роль <PlusOutlined />
+              </Button>
+            </Dropdown>
+        </div>
+     ); 
     }
 }
 
